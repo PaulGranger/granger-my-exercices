c40f731a4f48252b809e4ab4742089e1e8a1c93f
root <root@LAPTOP-S5MOAH7Q.localdomain>
2022-04-20 14:47:48 +0200

    tests

--- day-1/exercise-6.js
+++ day-1/exercise-6.js
@@ -1,14 +1,8 @@
 export const my_length_array = (str) => {
-	if (typeof str != 'string')
-	{
-		return [];
-	}
-
 	let i = 0;
-	while (str[i]) 
-	{
+	while (str[i]) {
 		i++;
 	}
 
-	return res;
+	return i;
 }
